87b9a0483c23e202d7239f10c22a828a3bafa500
2048JiaLi <992130182@qq.com>
2019-11-21 16:56:08 +0800

    update

--- 爬虫/网络爬虫入门到放弃/4-3.py
+++ 爬虫/网络爬虫入门到放弃/4-3.py
@@ -30,4 +30,6 @@ print(content.text)
 http://www.santostang.com/2018/07/15/4-3-%E9%80%9A%E8%BF%87selenium-%E6%A8%A1%E6%8B%9F%E6%B5%8F%E8%A7%88%E5%99%A8%E6%8A%93%E5%8F%96/
 
 使用Selenium抓取所有评论及一些操作
+
+> selenium -- https://github.com/2048JiaLi/my-learning-100days/blob/master/1121-day20.md
 '''
